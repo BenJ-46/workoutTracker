2abb84a209496276d1ebb44977acc007914c3659
bennaayy <jimenezbenjamin338@gmail.com>
2020-09-10 19:07:49 -0700

    it is what it is

--- models/Workout.js
+++ models/Workout.js
@@ -1,11 +1,53 @@
-const {model,Shema, Schema} = require('mongoose')
+const { model, Schema } = require("mongoose");
 
-const Workout = new Schema({
-    text:{
-        type: String,
-        unique: true,
-        required: true
+const Workout = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-}, { timestamps: true })
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          require: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+          require: true,
+        },
+        duration: {
+          type: Number,
+          require: true,
+        },
+        weight: {
+          type: Number,
+          require: false,
+        },
+        reps: {
+          type: Number,
+          require: false,
+        },
+        sets: {
+          type: Number,
+          require: false,
+        },
+        distance: {
+          type: Number,
+          require: false,
+        },
+      },
+    ],
+  },
+  { toJSON: { virtuals: true } }
+);
 
-module.exports = model('Workout', Workout)
+Workout.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+module.exports = model("Workout", Workout);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const logger = require("morgan");
 const { join } = require('path')
 
 const app = express()
@@ -9,6 +10,10 @@ app.use(express.json())
 
 app.use(require('./routes'))
 
-require('./db')
-  .then(() => app.listen(3000))
-  .catch(err => console.log(err))
+require("./db")
+  .then(() =>
+    app.listen(process.env.PORT || 3000, function () {
+      console.log("The Server Is Running At Port 3000.....");
+    })
+  )
+  .catch((err) => console.log(err));
